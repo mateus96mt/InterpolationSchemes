357cd67de57bfcaa9704bb925ab0fac451ae444a
Mateus Teixeira Magalhães <mateus.teixeira@haizen.tech>
2020-10-30 14:14:21 -0300

    Adicionando suporte para equacao da camada limite

--- main.py
+++ main.py
@@ -4,6 +4,22 @@
 import matplotlib.pyplot as plt
 import math
 import os
+import numpy as np
+import enum
+
+#class of equations of 'advection_difusion_equation_solver'
+class Equation_types(enum.Enum):
+    
+    Burges = 'burges'
+    Boundary_layer = 'boundary_layer'
+   
+#-----------------------------------ANALITIC SOLUTIONS------------------------
+
+def analitic_boundary_layer_equation(x, v):
+    
+    return (1 - np.exp(x / v)) / (1 - np.exp(1 / v))
+
+#-----------------------------------------------------------------------------
 
 #normalized variable
 def norm_var(PHI, PHI_D, PHI_R):
@@ -96,11 +112,11 @@ def U_G(U, i):
     return (U[i] + U[i-1]) / 2
 
 #approximation of value 'u' in face 'f' of node 'i'
-def u_f(u, i, SCHEME, param):
+def u_f(u, i, SCHEME, param, VEL_F):
     
     U, D, R = 0, 0, 0
     
-    if( U_F(u, i) > 0 ):
+    if( VEL_F > 0 ):
         
         D = u[i+1]
         R = u[i-1]
@@ -123,11 +139,11 @@ def u_f(u, i, SCHEME, param):
     return SCHEME(U, D, R, param)
 
 #approximation of value 'u' in face 'g' of node 'i'
-def u_g(u, i, SCHEME, param):
+def u_g(u, i, SCHEME, param, VEL_G):
     
     U, D, R = 0, 0, 0
     
-    if( U_G(u, i) > 0 ):
+    if( VEL_G > 0 ):
         
         D = u[i]
         
@@ -153,8 +169,8 @@ def apply_boundary_condition(u, uL, uR):
     
     u[0] = uL
     u[-1] = uR
-    
-def apply_initial_condition(u, domx):
+        
+def apply_initial_condition(u, domx, equation_type =Equation_types.Burges):
     
     n = len(u)
     
@@ -164,11 +180,18 @@ def apply_initial_condition(u, domx):
         
         x = domx[0] + i * d
         
-        u[i] = math.sin(2 * math.pi * x)
+        if equation_type == Equation_types.Burges:
+            
+            u[i] = math.sin(2 * math.pi * x)
+            
+        if equation_type == Equation_types.Boundary_layer:
+            
+            u[i] = 0
         
 
-def burges_equation_solver(nx, dx, domx, domt, cfl, v, SCHEME, param,\
-                           SCHEME_LABEL, marker = '.', PATH = 'results/'):
+def advection_difusion_equation_solver(nx, dx, domx, domt, cfl, v, SCHEME, param, \
+                                       SCHEME_LABEL, marker = '.', PATH = 'results/',
+                                       equation_type = Equation_types.Burges, a = 1):
         
     #time discretization size
     dt = cfl * dx
@@ -182,11 +205,23 @@ def burges_equation_solver(nx, dx, domx, domt, cfl, v, SCHEME, param,\
     #index of layer in matrix 'M_u' that stores 'u' at time 'i' and 'i + 1'
     p, q = 0, 1
     
-    apply_initial_condition(M_u[p], domx)
-    apply_initial_condition(M_u[q], domx)
+    apply_initial_condition(M_u[p], domx, equation_type)
+    apply_initial_condition(M_u[q], domx, equation_type)
     
-    apply_boundary_condition(M_u[p], 0, 0)
-    apply_boundary_condition(M_u[q], 0, 0)
+    uL, uR = 0, 0
+    
+    if equation_type == Equation_types.Burges:
+        
+        uL = 0
+        uR = 0
+        
+    if equation_type == Equation_types.Boundary_layer:
+        
+        uL = 0
+        uR = 1
+        
+    apply_boundary_condition(M_u[p], uL, uR)
+    apply_boundary_condition(M_u[q], uL, uR)
         
     params_log(nx, dx, nt, dt, domx, domt, cfl, v, param)
     
@@ -199,16 +234,36 @@ def burges_equation_solver(nx, dx, domx, domt, cfl, v, SCHEME, param,\
         #loop in space
         for i in range(1, nx - 1):
                 
-            UF = U_F(M_u[q], i)
-            UG = U_G(M_u[q], i)
+            #advection velocities in face f and g, respectively
+            VEL_F, VEL_G = 0, 0
             
-            uf = u_f(M_u[q], i, SCHEME, param)
-            ug = u_g(M_u[q], i, SCHEME, param)
+            if equation_type == Equation_types.Burges:
             
-            M_u[p][i] = ( dt * v * ( M_u[q][i-1] - 2 * M_u[q][i] + M_u[q][i+1] )\
-               / (dx ** 2) )\
-             - ( dt * 0.5 * ( (UF * uf) - (UG * ug) ) / dx )\
-             + M_u[q][i]
+                VEL_F = UF = U_F(M_u[q], i)
+                VEL_G = UG = U_G(M_u[q], i)
+            
+            if equation_type == Equation_types.Boundary_layer:
+                
+                VEL_F = a
+                VEL_G = a
+            
+            uf = u_f(M_u[q], i, SCHEME, param, VEL_F)
+            ug = u_g(M_u[q], i, SCHEME, param, VEL_G)
+            
+            if equation_type == Equation_types.Burges:
+            
+                M_u[p][i] = ( dt * v * ( M_u[q][i-1] - 2 * M_u[q][i] + M_u[q][i+1] )\
+                   / (dx ** 2) )\
+                 - ( dt * 0.5 * ( (UF * uf) - (UG * ug) ) / dx )\
+                 + M_u[q][i]
+                 
+            if equation_type == Equation_types.Boundary_layer:
+                
+                M_u[p][i] = ( dt * v * ( M_u[q][i-1] - 2 * M_u[q][i] + M_u[q][i+1] )\
+                   / (dx ** 2) )\
+                 - ( dt * a * (uf -  ug) / dx )\
+                 + M_u[q][i]
+            
         
         #save result in last iteration
         if t == nt-1:
@@ -221,7 +276,8 @@ def burges_equation_solver(nx, dx, domx, domt, cfl, v, SCHEME, param,\
                         SCHEME_LABEL, marker)
         
 #plot result and save in figure
-def save_result(domx, nx, dx, u, fileName, time, v, cfl, label, marker = '.'):
+def save_result(domx, nx, dx, u, fileName, time, v, cfl, label, 
+                marker = '.', ylimD = -2, ylimU = 2):
     
     #create folder if not exist
     if not os.path.exists(fileName.split('/')[0]):
@@ -232,7 +288,7 @@ def save_result(domx, nx, dx, u, fileName, time, v, cfl, label, marker = '.'):
             + '  cfl = ' + str(cfl)
     plt.title(title)
     
-    plt.ylim([-2, 2])
+    plt.ylim([ylimD, ylimU])
     
     plt.xlabel('x')
     plt.ylabel('u')
@@ -262,7 +318,7 @@ def params_log(nx, dx, nt, dt, domx, domt, cfl, v, param):
     print("param:", param)
     print("------------------------------------")
 
-def main():
+def TRAB1_CFD_MESTRADO():
     
     #domain in x direction
     domx = [0, 1]
@@ -291,7 +347,7 @@ def main():
     #plot and save initial condition
     initial_condition = [0 for i in range(nx)]
     apply_initial_condition(initial_condition, domx)
-    name = 'results/initial.png'
+    name = 'results_TRAB1_CFD_MESTRADO/initial.png'
     save_result(domx, nx, dx,\
                 initial_condition,\
                 name, 0, '-', '-', 'Condicao inicial')
@@ -303,7 +359,7 @@ def main():
     #-----------------------EXERCISE 2-----------------------
     
     #path to save results
-    PATH = 'results/EXE2_'
+    PATH = 'results_TRAB1_CFD_MESTRADO/EXE2_'
     
     #viscosity
     v = 0.001
@@ -316,17 +372,17 @@ def main():
             SCHEME = FSFL
             SCHEME_LABEL = 'FSFL'
             param = beta
-            burges_equation_solver(nx, dx, domx, domt, cfl, v,\
-                                   SCHEME, param, SCHEME_LABEL, marker = '.',\
-                                   PATH = PATH)
+            advection_difusion_equation_solver(nx, dx, domx, domt, cfl, v, \
+                                               SCHEME, param, SCHEME_LABEL, marker = '.', \
+                                               PATH = PATH)
             
             #seting upwind scheme, label and parameter
             SCHEME = ADBQUICKEST
             SCHEME_LABEL = 'ADBQUICKEST'
             param = cfl
-            burges_equation_solver(nx, dx, domx, domt, cfl, v,\
-                                   SCHEME, param, SCHEME_LABEL, marker = None,\
-                                   PATH = PATH)
+            advection_difusion_equation_solver(nx, dx, domx, domt, cfl, v, \
+                                               SCHEME, param, SCHEME_LABEL, marker = None, \
+                                               PATH = PATH)
             
             #clean plot
             plt.cla()
@@ -338,7 +394,7 @@ def main():
     #-----------------------EXERCISE 3-----------------------
     
     #path to save results
-    PATH = 'results/EXE3_'
+    PATH = 'results_TRAB1_CFD_MESTRADO/EXE3_'
     
     #Courant number
     cfl = 0.5
@@ -351,17 +407,17 @@ def main():
             SCHEME = FSFL
             SCHEME_LABEL = 'FSFL'
             param = beta
-            burges_equation_solver(nx, dx, domx, domt, cfl, v,\
-                                   SCHEME, param, SCHEME_LABEL, marker = '.',\
-                                   PATH = PATH)
+            advection_difusion_equation_solver(nx, dx, domx, domt, cfl, v, \
+                                               SCHEME, param, SCHEME_LABEL, marker = '.', \
+                                               PATH = PATH)
             
             #seting upwind scheme, label and parameter
             SCHEME = ADBQUICKEST
             SCHEME_LABEL = 'ADBQUICKEST'
             param = cfl
-            burges_equation_solver(nx, dx, domx, domt, cfl, v,\
-                                   SCHEME, param, SCHEME_LABEL, marker = None,\
-                                   PATH = PATH)
+            advection_difusion_equation_solver(nx, dx, domx, domt, cfl, v, \
+                                               SCHEME, param, SCHEME_LABEL, marker = None, \
+                                               PATH = PATH)
             
             #clean plot
             plt.cla()
@@ -369,7 +425,54 @@ def main():
     
     #--------------------------------------------------------
 
+
+def boundary_layer_equation_test():
     
-#calling main function
-main()
+    cfl = 0.1
+    
+    nx = 100
+    
+    v = 0.25
+    
+    domx = [0, 1]
+    
+    domt = [0, 1]
+    
+    dx = (domx[-1] - domx[0]) / (nx-1)
+ 
+    PATH = 'test/'
+    
+    n_v = 1
+    
+    beta = 0
+    
+    for i in range(n_v):
         
+        analitic = [analitic_boundary_layer_equation(i*dx, v) for i in range(nx)]
+        
+        fileName = PATH + 'boundary_layer'\
+            + '_time=' + str(0)\
+            + '_v=' + str(v)\
+            + '_cfl=' + str(cfl) + '.png'
+        
+        save_result(domx, nx, dx, analitic, fileName, 0, v, cfl, 
+                'analitica', marker = None, ylimD=-0.5, ylimU=1.5)
+        
+        SCHEME = FSFL
+        SCHEME_LABEL = 'FSFL'
+        param = beta
+        
+        advection_difusion_equation_solver(nx, dx, domx, domt, cfl, v, \
+                   SCHEME, param, SCHEME_LABEL, marker = '.', \
+                   PATH = PATH, equation_type = Equation_types.Boundary_layer)
+        
+        
+        #clean plot
+        plt.cla()
+        plt.clf()
+        
+        v = v / 10
+        
+#calling main function
+TRAB1_CFD_MESTRADO()
+#boundary_layer_equation_test()
